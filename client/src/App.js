b1ca52002cd8a495ba0fac0dbdc56e281c2c6a42
aisav <artur.isaverdyan@gmail.com>
2018-05-30 08:42:13 -0700

    main components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Landing from './components/layout/Landing'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <h1>React</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Navbar/>
+                <Landing/>
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
