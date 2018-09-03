48322461d8ab2f112edaa8d2f44cfbf2ab4aeb02
aisav <artur.isaverdyan@gmail.com>
2018-09-03 20:17:18 +0400

    deploy fixing

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,13 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys_prod.js')
+module.exports = {
+    mongoURI: 'mongodb://artur:artur@ds237989.mlab.com:37989/devconnector',
+    secretOrKey: 'secret'
 }
-else {
-    module.exports = require('./keys_dev.js')
 
-}
+
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./keys_prod')
+// }
+// else {
+//     module.exports = require('./keys_dev')
+//
+// }
