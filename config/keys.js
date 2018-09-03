b72c1c90a11da45a3b87a21861dcf18406ac5f58
aisav <artur.isaverdyan@gmail.com>
2018-09-03 18:15:24 +0400

    envoirements config

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
-module.exports = {
-  mongoURI: 'mongodb://artur:artur@ds237989.mlab.com:37989/devconnector',
-  secretOrKey: 'secret'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod')
+}
+else {
+    module.exports = require('./keys_dev')
+
 }
